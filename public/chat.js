7fb32e0eedef53766703c81aebe2e39d2194708c
Mory Keita <morykeita@protonmail.com>
2018-08-24 11:54:00 -0400

    enabled websocket chat

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,32 @@
 // Make client side socket connection
 
 var socket = io.connect('http://localhost:4000');
+
+var chatMessage = document.getElementById('chat-message');
+var chatHandle = document.getElementById('chat-handle');
+var chatOutput = document.getElementById('chat-output');
+var sendBtn = document.getElementById('send-buttom');
+var chatFeedback = document.getElementById('chat-feedback');
+
+// emit event
+
+sendBtn.addEventListener('click',()=>{
+    socket.emit('chat',{
+        message : chatMessage.value,
+        handle : chatHandle.value
+    })
+});
+
+chatMessage.addEventListener('keypress', () =>{
+    socket.emit('typing',chatHandle.value);
+})
+
+// listen for events
+socket.on('chat',(data)=>{
+    chatOutput.innerHTML += '<p> <strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    chatFeedback.innerHTML ='';
+})
+
+socket.on('typing',(data) =>{
+    chatFeedback.innerHTML = '<p> <em>' + data + 'is typing...</em> </p>';
+})
--- server.js
+++ server.js
@@ -19,4 +19,12 @@ app.use(express.static('public'));
 var io = socket(server);
 io.on('connection', (socket)=>{
     console.log("made socket connection", socket.id);
+
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',(data)=>{
+        socket.broadcast.emit('typing',data);
+    });
 })
